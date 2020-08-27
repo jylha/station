645aa8135713c77cd10b5d3626d13b8f4efb118c
Juhani Jylhä <jylha@users.noreply.github.com>
2020-08-27 13:39:12 +0300

    Add Scaffold and LazyColumn for SelectStationScreen.

--- app/src/main/java/com/example/station/ui/select/SelectStationScreen.kt
+++ app/src/main/java/com/example/station/ui/select/SelectStationScreen.kt
@@ -2,26 +2,65 @@ package com.example.station.ui.select
 
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.viewModel
 import com.example.station.Screen
+import com.example.station.model.Station
 
 @Composable
 fun SelectStationScreen(
     navigateTo: (Screen) -> Unit
 ) {
-    val viewModel =  viewModel<SelectStationsViewModel>()
+
+    val viewModel = viewModel<SelectStationsViewModel>()
     val stations by viewModel.stations.observeAsState()
-    Column {
-        Text("Select station")
-        stations?.map { station ->
-            Text(station.name, modifier = Modifier.clickable(onClick = {
-                navigateTo(Screen.Timetable(station.name))
-            }))
+
+    Scaffold(
+        topBar = {
+            TopAppBar {
+                Text(modifier = Modifier.padding(16.dp), text = "Select station")
+            }
         }
+    ) {
+        if (stations != null) {
+            StationList(stations!!, onSelect = { station ->
+                navigateTo(Screen.Timetable(station.name))
+            })
+        } else {
+            LoadingStations()
+        }
+    }
+}
+
+@Composable
+fun StationList(stations: List<Station>, onSelect: (Station) -> Any) {
+    LazyColumnFor(stations) { station ->
+        Text(
+            text = station.name,
+            modifier = Modifier
+                .clickable(onClick = { onSelect(station) })
+                .padding(8.dp)
+        )
+    }
+}
+
+
+@Composable
+fun LoadingStations() {
+    Surface(Modifier.fillMaxSize()) {
+        Text(
+            text = "Loading stations...",
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
