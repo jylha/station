4f8bfc2c1c45e456500c45a62f3e5e45dad42c11
Juhani Jylhä <jylha@users.noreply.github.com>
2020-09-03 20:54:10 +0300

    Add parameters for train query.

--- app/src/main/java/com/example/station/data/timetable/network/TimetableService.kt
+++ app/src/main/java/com/example/station/data/timetable/network/TimetableService.kt
@@ -2,10 +2,16 @@ package com.example.station.data.timetable.network
 
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TimetableService {
 
-    @GET("live-trains/station/{stationCode}?minutes_before_departure=15&minutes_after_departure=15&minutes_before_arrival=15&minutes_after_arrival=15")
-    suspend fun fetchTimetable(@Path("stationCode") stationCode: String): List<TrainNetworkEntity>
-
+    @GET("live-trains/station/{stationCode}")
+    suspend fun fetchTimetable(
+        @Path("stationCode") stationCode: String,
+        @Query("minutes_before_departure") minutesBeforeDeparture: Int = 120,
+        @Query("minutes_after_departure") minutesAfterDeparture: Int = 30,
+        @Query("minutes_before_arrival") minutesBeforeArrival: Int = 120,
+        @Query("minutes_after_arrival") minutesAfterArrival: Int = 30
+    ): List<TrainNetworkEntity>
 }
