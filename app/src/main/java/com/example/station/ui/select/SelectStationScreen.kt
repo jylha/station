1d3d5f9253b274e91254f5600669a6477734f3d0
Juhani Jylhä <jylha@users.noreply.github.com>
2020-09-04 22:14:21 +0300

    Highlight search string in station names.

--- app/src/main/java/com/example/station/ui/select/SelectStationScreen.kt
+++ app/src/main/java/com/example/station/ui/select/SelectStationScreen.kt
@@ -1,13 +1,16 @@
 package com.example.station.ui.select
 
+import androidx.compose.foundation.Box
 import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.TopAppBar
@@ -20,6 +23,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.viewModel
 import com.example.station.Screen
@@ -64,9 +69,12 @@ fun SelectStationScreen(
         }
     ) { innerPadding ->
         if (stations != null) {
-            StationList(filteredStations, onSelect = { station ->
-                navigateTo(Screen.Timetable(station))
-            }, Modifier.padding(innerPadding))
+            StationList(
+                filteredStations,
+                onSelect = { station -> navigateTo(Screen.Timetable(station)) },
+                Modifier.padding(innerPadding),
+                highlightedText = searchText
+            )
         } else {
             LoadingStations()
         }
@@ -74,20 +82,44 @@ fun SelectStationScreen(
 }
 
 @Composable
-fun StationList(
+private fun StationList(
     stations: List<Station>,
     onSelect: (Station) -> Any,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    highlightedText: String? = null
 ) {
     Column(modifier) {
         LazyColumnFor(stations) { station ->
-            Text(
-                text = station.name,
-                modifier = Modifier
-                    .clickable(onClick = { onSelect(station) })
-                    .padding(8.dp)
-            )
+            StationListEntry(station = station, onSelect = onSelect, searchText = highlightedText)
         }
+    }
+}
+
+@Composable
+private fun StationListEntry(
+    station: Station,
+    onSelect: (Station) -> Any,
+    modifier: Modifier = Modifier,
+    searchText: String? = null
+) {
+    val fg = MaterialTheme.colors.onPrimary
+    val bg = MaterialTheme.colors.primary
+    val name = remember(station.name, searchText) {
+        with(AnnotatedString.Builder(station.name)) {
+            if (searchText?.isNotBlank() == true) {
+                val index = station.name.indexOf(searchText, 0, ignoreCase = true)
+                addStyle(SpanStyle(color = fg, background = bg), index, index + searchText.length)
+            }
+            toAnnotatedString()
+        }
+    }
+    Box(
+        modifier
+            .fillMaxWidth()
+            .clickable(onClick = { onSelect(station) })
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(name)
     }
 }
 
