6d8a2828696b4a497bf0a3ff22c6a64370c92d6b
Juhani Jylhä <jylha@users.noreply.github.com>
2020-09-19 13:21:05 +0300

    Add train animation to welcome screen.

--- app/src/main/java/com/example/station/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/station/ui/home/HomeScreen.kt
@@ -1,12 +1,11 @@
 package com.example.station.ui.home
 
+import android.content.Context
 import androidx.compose.foundation.Box
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -15,12 +14,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.viewinterop.viewModel
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 import com.example.station.ui.Screen
 import com.example.station.ui.components.LoadingMessage
 import com.example.station.ui.theme.blue
@@ -47,10 +52,8 @@ fun HomeScreen(
     }
 }
 
-@Composable
-private fun WelcomeCard(onSelect: () -> Unit) {
+@Composable private fun WelcomeCard(onSelect: () -> Unit = {}) {
     Card(
-
         Modifier
             .padding(8.dp)
             .clip(RoundedCornerShape(16.dp))
@@ -61,10 +64,27 @@ private fun WelcomeCard(onSelect: () -> Unit) {
             verticalArrangement = Arrangement.Center
         ) {
             Text("Welcome!", style = MaterialTheme.typography.h4)
-            Spacer(modifier = Modifier.height(16.dp))
+            WelcomeAnimation(Modifier.padding(20.dp))
             Button(onClick = onSelect) {
                 Text("Select station")
             }
         }
     }
 }
+
+@Composable private fun WelcomeAnimation(modifier: Modifier = Modifier) {
+    var animationView by remember { mutableStateOf<LottieAnimationView?>(null) }
+
+    AndroidView({ context: Context ->
+        val view = LottieAnimationView(context)
+        view.setAnimation("train-animation.json")
+        view.repeatCount = 10
+        view.repeatMode = LottieDrawable.RESTART
+        view.playAnimation()
+        animationView = view
+        view
+    },
+        modifier,
+        {})
+}
+
