5c2ea2350184f0d728ae9015d50b2fdc664d818e
Juhani Jylhä <jylha@users.noreply.github.com>
2022-02-01 18:03:53 +0200

    Update Compose to 1.1.0-rc03, Kotlin to 1.6.10, and some other gradle dependencies.

--- app/src/main/java/dev/jylha/station/data/location/DefaultLocationService.kt
+++ app/src/main/java/dev/jylha/station/data/location/DefaultLocationService.kt
@@ -3,25 +3,23 @@ package dev.jylha.station.data.location
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
+import android.os.Looper
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Inject
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.tasks.await
-import javax.inject.Inject
 
-
-@OptIn(ExperimentalCoroutinesApi::class)
 class DefaultLocationService @Inject constructor(
     @ApplicationContext private val context: Context
 ) : LocationService {
 
-    private val client = LocationServices.getFusedLocationProviderClient(context)
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
     @SuppressLint("MissingPermission")
     override fun currentLocation(): Flow<Location> {
@@ -31,16 +29,17 @@ class DefaultLocationService @Inject constructor(
             locationRequest.maxWaitTime = 1000
 
             val locationCallback = object : LocationCallback() {
-                override fun onLocationResult(result: LocationResult?) {
-                    result?.locations?.forEach { location ->
+                override fun onLocationResult(result: LocationResult) {
+                    result.locations.forEach { location ->
                         runCatching { this@channelFlow.trySend(location).isSuccess }
                     }
                 }
             }
-            @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-            client.requestLocationUpdates(locationRequest, locationCallback, null).await()
-            awaitClose { client.removeLocationUpdates(locationCallback) }
+
+            fusedLocationClient
+                .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+                .await()
+            awaitClose { fusedLocationClient.removeLocationUpdates(locationCallback) }
         }
     }
 }
-
