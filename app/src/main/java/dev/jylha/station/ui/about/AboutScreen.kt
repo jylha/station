5ca2fac89f9bd8b2698a6dddeff28cee5d319553
Juhani Jylhä <jylha@users.noreply.github.com>
2023-10-28 19:07:34 +0300

    Migrate AboutScreen to use the M3 theme version

--- app/src/main/java/dev/jylha/station/ui/about/AboutScreen.kt
+++ app/src/main/java/dev/jylha/station/ui/about/AboutScreen.kt
@@ -9,15 +9,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,47 +41,44 @@ fun AboutScreen(
     val informationSourceText = stringResource(R.string.text_information_source)
     val trainAnimationCreditText = stringResource(R.string.text_train_animation_credit)
 
-    val backgroundColor = with(MaterialTheme.colors) {
-        if (isLight) primary else background
-    }
-    val surfaceColor = MaterialTheme.colors.surface
-    val contentColor = MaterialTheme.colors.onSurface.copy(alpha = 0.8f)
-        .compositeOver(surfaceColor)
-    val textStyle = MaterialTheme.typography.body1.copy(
-        lineHeight = MaterialTheme.typography.body1.fontSize * 1.5
+    val textStyle = MaterialTheme.typography.bodyLarge.copy(
+        lineHeight = MaterialTheme.typography.bodyLarge.fontSize * 1.5
     )
 
     Card(
         modifier = Modifier
             .fillMaxSize()
-            .background(backgroundColor)
+            .background(MaterialTheme.colorScheme.background)
             .padding(8.dp)
-            .clip(RoundedCornerShape(16.dp))
-            .fillMaxSize(),
-        backgroundColor = surfaceColor,
-        contentColor = contentColor,
+            .clip(RoundedCornerShape(16.dp)),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface,
+        ),
     ) {
         Box(
-            Modifier.padding(
-                horizontal = 20.dp,
-                vertical = if (portraitOrientation()) 40.dp else 20.dp
-            ),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    horizontal = 20.dp,
+                    vertical = if (portraitOrientation()) 40.dp else 20.dp
+                ),
         ) {
             Column(
                 modifier = Modifier.fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text(aboutLabel, style = MaterialTheme.typography.h5)
+                Text(aboutLabel, style = MaterialTheme.typography.headlineMedium)
                 Text(
                     "$informationSourceLabel $informationSourceText",
-                    color = contentColor,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
                     textAlign = TextAlign.Center,
                     style = textStyle
                 )
                 Text(
                     trainAnimationCreditText,
-                    color = contentColor,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
                     textAlign = TextAlign.Center,
                     style = textStyle
                 )
@@ -90,7 +87,10 @@ fun AboutScreen(
                 onClick = onNavigateToOssLicenses,
                 modifier = Modifier.align(Alignment.BottomCenter)
             ) {
-                Text(text = stringResource(R.string.label_oss_licenses))
+                Text(
+                    text = stringResource(R.string.label_oss_licenses),
+                    style = MaterialTheme.typography.bodyLarge,
+                )
             }
         }
     }
--- app/src/main/java/dev/jylha/station/ui/theme/StationColorScheme.kt
+++ app/src/main/java/dev/jylha/station/ui/theme/StationColorScheme.kt
@@ -6,16 +6,35 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
 private val White = Color(0xFFFFFFFF)
+private val LightGray1 = Color(0xFFf8f9fa)
+private val LightGray2 = Color(0xFFe9ecef)
+private val LightGray3 = Color(0xFFdee2e6)
+private val LightGray4 = Color(0xFFced4da)
+private val Gray = Color(0xFFadb5bd)
+private val DarkGray4 = Color(0xFF6c757d)
+private val DarkGray3 = Color(0xFF495057)
+private val DarkGray2 = Color(0xFF343a40)
+private val DarkGray1 = Color(0xFF212529)
 private val Black = Color(0xFF000000)
 
 private val Red = Color(0xFFD32F2F)
 private val Blue = Color(0xFF005DB8)
 private val LightBlue = Color(0xFF1976D2)
-private val LightGray = Color(0xFFF2F2F2)
-private val DarkGray = Color(0xFF121212)
 
 /** A dark color scheme for the application. */
-val StationDarkColorScheme = darkColorScheme()
+val StationDarkColorScheme = darkColorScheme(
+    primary = LightBlue,
+    surface = DarkGray1,
+    onSurface = White,
+    onSurfaceVariant = LightGray2,
+    background = Black,
+)
 
 /** A light color scheme for the application. */
-val StationLightColorScheme = lightColorScheme()
+val StationLightColorScheme = lightColorScheme(
+    primary = Blue,
+    surface = White,
+    onSurface = Black,
+    onSurfaceVariant = DarkGray2,
+    background = Blue,
+)
