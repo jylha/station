ab7b6a513acfcf30691e81d5d0acecb00dd58038
Juhani Jylhä <jylha@users.noreply.github.com>
2023-10-28 17:46:23 +0300

    Add compose-material3 dependency and add a Material3 version of the app theme.

---
+++ app/src/main/java/dev/jylha/station/ui/theme/StationColorScheme.kt
@@ -0,0 +1,21 @@
+
+package dev.jylha.station.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+private val White = Color(0xFFFFFFFF)
+private val Black = Color(0xFF000000)
+
+private val Red = Color(0xFFD32F2F)
+private val Blue = Color(0xFF005DB8)
+private val LightBlue = Color(0xFF1976D2)
+private val LightGray = Color(0xFFF2F2F2)
+private val DarkGray = Color(0xFF121212)
+
+/** A dark color scheme for the application. */
+val StationDarkColorScheme = darkColorScheme()
+
+/** A light color scheme for the application. */
+val StationLightColorScheme = lightColorScheme()
