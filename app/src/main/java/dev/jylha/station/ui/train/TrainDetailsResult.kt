09a555c43b7ecc9478fd02b9fcfa5b49ea8dee6c
Juhani Jylhä <jylha@users.noreply.github.com>
2024-01-21 22:04:33 +0200

    Change TrainDetailsResult objects to data objects

--- app/src/main/java/dev/jylha/station/ui/train/TrainDetailsResult.kt
+++ app/src/main/java/dev/jylha/station/ui/train/TrainDetailsResult.kt
@@ -8,27 +8,21 @@ sealed class TrainDetailsResult
 
 /** Results for loading train details.. */
 sealed class LoadTrainDetails : TrainDetailsResult() {
-    object Loading : LoadTrainDetails() {
-        override fun toString(): String = "LoadTrainDetails.Loading"
-    }
+    data object Loading : LoadTrainDetails()
     data class Error(val message: String?) : LoadTrainDetails()
     data class Success(val train: Train) : LoadTrainDetails()
 }
 
 /** Results for reloading train details. */
 sealed class ReloadTrainDetails : TrainDetailsResult() {
-    object Loading : ReloadTrainDetails() {
-        override fun toString(): String = "ReloadTrainDetails.Loading"
-    }
+    data object Loading : ReloadTrainDetails()
     data class Error(val message: String?) : ReloadTrainDetails()
     data class Success(val train: Train) : ReloadTrainDetails()
 }
 
 /** Results for loading station name mapper. */
 sealed class LoadNameMapper : TrainDetailsResult() {
-    object Loading : LoadNameMapper() {
-        override fun toString(): String = "LoadNameMapper.Loading"
-    }
+    data object Loading : LoadNameMapper()
     data class Error(val message: String?) : LoadNameMapper()
     data class Success(val mapper: StationNameMapper) : LoadNameMapper()
 }
