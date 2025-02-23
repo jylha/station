d4a57c6aab4524471e8c821fbc2a74fa47fd49b6
Juhani Jylhä <jylha@users.noreply.github.com>
2025-02-23 14:17:59 +0200

    Version 1.2.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.jylha.station"
         minSdk = libs.versions.min.sdk.get().toInt()
         targetSdk = libs.versions.target.sdk.get().toInt()
-        versionCode = 26
-        versionName = "1.2.13"
+        versionCode = 27
+        versionName = "1.2.14"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations += listOf("en", "fi-rFI", "sv-rFI")
     }
